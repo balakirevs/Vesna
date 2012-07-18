9718daabd98556f45585659f15fa997a986b0463
Alexander Balakirev <balakirevs@i.ua>
2012-07-18 11:52:54 +0300

    Add a Pages controller

---
+++ README.md
@@ -0,0 +1,1 @@
+This is Social Net Vesna.
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Vesna::Application.routes.draw do
+  get "pages/home"
+
+  get "pages/about"
+
+  get "pages/rules"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
