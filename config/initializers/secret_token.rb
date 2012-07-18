3cfb183c94f4715209c7c9c6c4e84fa7cb5124e5
Alexander Balakirev <balakirevs@i.ua>
2012-07-18 11:26:19 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vesna::Application.config.secret_token = 'cf08880af174367a5f25e077b0951d35a2376ce12e3bfdcfbbfc24d401c5c05d8de03187b6e62f83ae2c4113ecc0385f398b763c56bb36a779e9358a6e6917ee'
